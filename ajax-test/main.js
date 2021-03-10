4d2f277f4d79c5293f388ce86767e8aa826b9d51
cartman1978 <cartman_7802@yahoo.ie>
2021-03-10 12:13:31 +0000

    added writedocument function

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,23 +1,23 @@
-function getData(cb) {
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
     let xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.open("GET", baseURL + type + "/");
 xhr.send();
 
 
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        // document.getElementById("data").innerHTML = this.responseText;
         cb(JSON.parse(this.responseText));
         
     }
   };
 }
 
-function printDataToConsole(data) {
-    console.log(data);
-    
+function writeToDocument(type) {
+    getData(type, function(data) {
+          document.getElementById("data").innerHTML = data;
+    });
 }
-
-getData(printDataToConsole);
