0bf6299dcfa75ff13d3222e4355b0f548d68bc00
cartman1978 <cartman_7802@yahoo.ie>
2021-03-10 11:16:06 +0000

    added callback function

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,5 +1,5 @@
-let xhr = new XMLHttpRequest();
-let data;
+function getData(cb) {
+    let xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
@@ -9,12 +9,15 @@ xhr.send();
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // document.getElementById("data").innerHTML = this.responseText;
-       data = JSON.parse(this.responseText);
+        cb(JSON.parse(this.responseText));
         
     }
-};
+  };
+}
 
-setTimeout(function() {
-   console.log(data);
-}, 500);
+function printDataToConsole(data) {
+    console.log(data);
+    
+}
 
+getData(printDataToConsole);
