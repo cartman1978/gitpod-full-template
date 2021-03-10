f9e165c7132facafb748ec253e45f53247f863e0
cartman1978 <cartman_7802@yahoo.ie>
2021-03-10 10:59:43 +0000

    added setTimeout function

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -4,18 +4,17 @@ let data;
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-    console.log(data);
-    
-}
+
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // document.getElementById("data").innerHTML = this.responseText;
-       setData(JSON.parse(this.responseText));
+       data = JSON.parse(this.responseText);
         
     }
 };
 
+setTimeout(function() {
+   console.log(data);
+}, 500);
 
