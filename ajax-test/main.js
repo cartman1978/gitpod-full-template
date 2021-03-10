1a53ad7a91961ec49a6781da509555eb8d87092b
cartman1978 <cartman_7802@yahoo.ie>
2021-03-10 13:38:20 +0000

    test

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -17,7 +17,13 @@ xhr.onreadystatechange = function() {
 }
 
 function writeToDocument(type) {
+    let el = document.getElementById("data");
+    el.innerHTML = "";
+
     getData(type, function(data) {
-          document.getElementById("data").innerHTML = data;
+          data = data.results;
+          data.forEach(function(item) {
+             el.innerHTML += "<p>" + item.name + "</p>";
+          });
     });
 }
